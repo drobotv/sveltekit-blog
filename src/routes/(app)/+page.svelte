bb907e1b9bbf6a848aa6a34b7f3205262cd4d892
drobotv <drobotvladik@gmail.com>
2024-07-11 19:34:06 +0200

    chore: remove home nav

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -7,14 +7,11 @@
 </script>
 
 <nav>
-  <ul class="flex space-x-4">
-    <li>
-      <Button variant="link">{m.relevant()}</Button>
-    </li>
-    <li>
-      <Button variant="link" disabled>{m.latest()}</Button>
-    </li>
-  </ul>
+  <div class="markdown-body mb-6 flex flex-col">
+    <div class="w-full text-center">
+      <h1 class="font-semibold">{m.latest()}</h1>
+    </div>
+  </div>
 </nav>
 <div class="grid grid-cols-1 gap-4">
   {#each data.posts as post}
